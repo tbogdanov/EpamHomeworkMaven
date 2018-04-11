d2320fb8892f222c60440254f16e8ba46c70a03d
tbogdanov <gion22@gmail.com>
2018-04-11 04:11:22 +0300

    - Removed unneeded methods and enums from OfficeDeskStats

--- src/main/java/org/tbogdanov/epamhw/module2/officesupplies/officedeskstats/OfficeDeskStats.java
+++ src/main/java/org/tbogdanov/epamhw/module2/officesupplies/officedeskstats/OfficeDeskStats.java
@@ -14,13 +14,6 @@ public class OfficeDeskStats {
 
     private List<OfficeDesk> deskList;
 
-    enum SortType {
-        BY_NAME, BY_PRICE, BY_PRICE_NAME;
-    }
-
-    enum SortManner {
-        ASCEND, DESCEND
-    }
 
     public OfficeDeskStats() {
         deskList = new LinkedList<>();
@@ -53,26 +46,6 @@ public class OfficeDeskStats {
 
     public List<OfficeDesk> getDeskList() {
         return deskList;
-    }
-
-    public void sortDesk(int i, SortType sortType, SortManner sortManner) throws InvalidParameterException {
-        if (i >= deskList.size()) {
-            throw new InvalidParameterException(String.format("Out of list (tried %d but size is %d)",
-                    i, deskList.size()));
-        }
-
-        boolean desc = (sortManner == SortManner.DESCEND);
-
-        switch (sortType) {
-            case BY_NAME:
-                deskList.get(i).sortSuppliesByName(desc);
-                break;
-            case BY_PRICE:
-                deskList.get(i).sortSuppliesByPrice(desc);
-                break;
-            case BY_PRICE_NAME:
-                deskList.get(i).sortSuppliesByPriceAndName(desc);
-        }
     }
 
 }
